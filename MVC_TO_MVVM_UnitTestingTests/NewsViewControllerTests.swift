d6d3fb2a1fb2b07bc4c05f3547716c5b6836afe3
ganeshpadole <ganeshpadole55@gmail.com>
2023-04-16 23:11:15 +0530

    check behaviour for sets title on viewDidLoad test

--- MVC_TO_MVVM_UnitTestingTests/NewsViewControllerTests.swift
+++ MVC_TO_MVVM_UnitTestingTests/NewsViewControllerTests.swift
@@ -25,9 +25,20 @@ class NewsViewControllerTests: XCTestCase {
         let initialVC = sb.instantiateInitialViewController()
         let navigation = try XCTUnwrap(initialVC as? UINavigationController)
         
-        let sut = try XCTUnwrap(navigation.topViewController as? NewsViewController)
+       _ = try XCTUnwrap(navigation.topViewController as? NewsViewController)
     }
 
-    
+    func test_viewDidLoad_setsTitle() throws {
+        let bundle = Bundle(for: NewsViewController.self)
+        let sb = UIStoryboard(name: "Main", bundle: bundle)
+        
+        let initialVC = sb.instantiateInitialViewController()
+        let navigation = try XCTUnwrap(initialVC as? UINavigationController)
+        
+        let sut = try XCTUnwrap(navigation.topViewController as? NewsViewController)
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.title, "News")
+    }
 
 }
